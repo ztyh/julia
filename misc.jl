c0e7da5cc2e965cea9ad6f13e6d189e5d399322e
ztyh <ztyh0121@gmail.com>
2018-10-30 19:54:41 -0400

    added quantiles

--- misc.jl
+++ misc.jl
@@ -1,3 +1,18 @@
 using Random
 using Distributions
 rand(Normal(2,1),10)
+-quantile(TDist(8),0.05)
+-quantile(TDist(26),0.005)
+-quantile(Normal(0,1),0.005)
+(1-cdf(Normal(0,1),2/sqrt(2.1)))*2
+
+X=rand(Normal(5,2),16)
+
+Y=rand(Normal(3,2),12)
+
+mean(X)
+
+mean(Y)
+
+var(X)
+var(Y)
